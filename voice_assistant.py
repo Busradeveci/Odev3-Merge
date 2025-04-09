fe6b2a7c4f89e61f2d7c4f11dd934470b0395892
Busra Deveci <devecibusra136@gmail.com>
2025-04-09 22:00:54 +0300

    İlk commit: Ses tanıma projesi ve kütüphaneler eklendi

---
+++ voice_assistant.py
@@ -0,0 +1,33 @@
+import speech_recognition as sr
+import pyttsx3
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+with sr.Microphone() as source:
+    print("Say something (like 'Hi' or 'By'): ")
+    audio = recognizer.listen(source)
+
+try:
+    text = recognizer.recognize_google(audio)
+    print("You said:", text)
+    if "hi" in text.lower():
+        print("Hi again!")
+        engine.say("Hi again!")
+        engine.runAndWait()
+    elif "by" in text.lower():
+        print("By now!")
+        engine.say("By now!")
+        engine.runAndWait()
+    else:
+        print("Hi")
+        engine.say("Hi")
+        engine.runAndWait()
+except sr.UnknownValueError:
+    print("Sorry, I couldnt understand what you said.")
+    engine.say("Sorry, I couldnt understand.")
+    engine.runAndWait()
+except sr.RequestError:
+    print("Sorry, there is a problem with the service.")
+    engine.say("Service error.")
+    engine.runAndWait()
